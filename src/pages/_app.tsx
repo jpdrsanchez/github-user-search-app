77ac9641988072cfef4a4444feb13eae5ec86b8a
jpdrsanchez <joaopedrosanchez302@gmail.com>
2021-12-06 12:17:27 -0300

    feat(app): add typography

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from 'next/app'
+import '@fontsource/kumbh-sans/400.css'
+import '@fontsource/kumbh-sans/700.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
