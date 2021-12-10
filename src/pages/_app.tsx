61d8958b8ebd166d71c3afc9217cf760a2ff674a
jpdrsanchez <joaopedrosanchez302@gmail.com>
2021-12-10 12:08:27 -0300

    refactor: frontendmentor reports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import '@fontsource/space-mono/400.css'
 import '@fontsource/space-mono/700.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Frontend Mentor | GitHub user search app</title>
+        <meta name="description" content="Search for any github user" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
